94b6057008ba07ec4904a645e97c46088baea279
pushthetemp <47386859+pushthetemp@users.noreply.github.com>
2019-03-30 22:23:29 +0000

    Add files via upload

---
+++ contacrt.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST[submit''])) {
+    $firstName = $_POST['firstname'];
+    $lastName = $_POST['lastname'];
+    $mobile = $_POST['mobile'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "alex@pushthetempo.co.uk";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an emailfrom".$firstName, $lastName.".\n\n". $message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.htmlmailsend");
+}
