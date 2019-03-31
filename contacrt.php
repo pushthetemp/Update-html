75109f0d06995b7b20eb3c14a2a35f88f1a3cbcd
pushthetemp <47386859+pushthetemp@users.noreply.github.com>
2019-03-31 00:45:02 +0000

    Update contacrt.php

--- contacrt.php
+++ contacrt.php
@@ -1,9 +1,6 @@
 <?php
-
- if (isset($_POST['submit'])) {
     $firstName = $_POST['firstname'];
     $lastName = $_POST['lastname'];
-    $mobile = $_POST['mobile'];
     $mailFrom = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
@@ -13,6 +10,7 @@
     $txt = "You have recieved an emailfrom".$firstName.".\n\n". $message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: contact.htmlmailsend");
+    header("Location: contact.html");
 }
+?>
 
